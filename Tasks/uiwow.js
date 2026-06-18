e197e3fb22eb8f76a4f5009d9d07565817cc86b8
ShenShang <shenshang@live.com>
2026-06-19 01:06:16 +0800

    删除 uiwow.js

--- Tasks/uiwow.js
+++
@@ -1,9 +0,0 @@
-/******************************************
- * @name UIWOW 旧脚本兼容
- * @description 旧版 Loon HTTP-REQUEST 规则可能仍指向本文件；本文件只静默退出。正式签到请使用 uiwow-sign.js。
- * @version 0.1.0
- ******************************************/
-
-if (typeof $done !== "undefined") {
-  $done({});
-}
